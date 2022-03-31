6009d9524c83efd8332a087f85dc4966578600ee
stefanoyoyo <git.stefano@outlook.it>
2022-03-31 17:11:14 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,23 +62,44 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     // Center of the united states
     const wktPoint = 'POINT(-102.96036309265152 42.34403599683588)'; 
+    const wktLine = 'POLYGON((10.689 -25.092, 34.595 ' +
+    '-20.170, 38.814 -35.639, 13.502 ' +
+    '-39.155, 10.689 -25.092))'; 
     
     const wkt = new WKT();
+
+    // Adding a point on map using its wkt
     const geometry = wkt.readGeometry(wktPoint, {
       dataProjection: 'EPSG:4326',
       featureProjection: 'EPSG:3857',
     });
     const position = (geometry as Point).getCoordinates();
+    const ov = new Overlay({
+      element: document.getElementById('square'),
+      position: position
+    });
+    this.map.addOverlay(ov);
+
+    // Adding a line on map using its wkt
+    // CAPIRE COME FARE 
+    const feature = wkt.readFeature(wktLine, {
+      dataProjection: 'EPSG:4326',
+      featureProjection: 'EPSG:3857',
+    })
+    const vector = new VectorLayer({
+      source: new VectorSource({
+        features: [feature],
+      }),
+    });
+    const ovLine = new Overlay({
+      element: document.getElementById('square'),
+      position: position
+    });
+    this.map.addOverlay(ovLine);
 
     // MEGLIO NON FARLO PERCHE' RITORNA PIU DI UN TIPO: number[], number[][] e number[][][] 
     // const pos = WktHelper.WktAsCoordinates(wktPoint, 'EPSG:4326', 'EPSG:3857');
 
 
-    const ov = new Overlay({
-      element: document.getElementById('square'),
-      position: position
-    });
-
-    this.map.addOverlay(ov);
   }
 }
